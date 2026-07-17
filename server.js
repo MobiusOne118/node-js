14a49aa431470fe107768c70b76053c88ddc6c6f
Chris Benham <christopher.benham@3plearning.com>
2026-07-17 15:35:56 +1000

    CORS added

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const port = 8080;
 // Using async here to mock promise.
 //
 const server = createServer(async (req, res) => {
-  res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+
 
   // Direct to route
   if(req.url.startsWith('/mechs')) {
     await mechsRoutes(req, res);
   } else {
-    res.writeHead(200);
+    res.writeHead(404);
     res.end('Server: No route selected');
   }
 });
